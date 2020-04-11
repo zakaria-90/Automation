33b0c5920848b129f4ee843233e0e58bd44abf9f
zakaria-90 <zak.omer.90@gmail.com>
2020-04-11 21:46:00 +0300

    Move and Error Print and Script File Name Don't Move

--- Organise-Directory-Files.py
+++ Organise-Directory-Files.py
@@ -5,14 +5,25 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 try:
     print("Organising your files intro [ images - music - video -executable - archive - torrent - document - code - design files]")
     for filename in os.listdir(dir_path):
+        # Check if files are code files
+        if filename.lower().endswith((".py", ".php", ".html" , ".css" , ".js")):
+            #Check if this file is Not The Script File itself
+            if not __file__.endswith(filename):
+                # If code folder doesnt exist then create
+                dir_name = "code"
+                if not os.path.exists(dir_name):
+                    os.makedirs(dir_name)
+                shutil.move(filename, dir_name)
+                #os.remove(filename)
+
         # Check if files are images and you can add more extentions 
-        if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".pbm", ".pnm")):
+        elif filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".pbm", ".pnm")):
             # If images folder doesnt exist then create new folder
             dir_name = "images"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
         # Check if files are music and you can add more extentions
         elif filename.lower().endswith((".wav", ".mp3", ".flac", ".3gp", ".aa", ".aax", ".aiff", ".raw")):
@@ -20,8 +31,8 @@ try:
             dir_name = "music"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
         # Check if files are videos and you can add more extentions
         elif filename.lower().endswith((".webm", ".mp4")):
@@ -29,8 +40,8 @@ try:
             dir_name = "video"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
         # Check if files are executables
         elif filename.lower().endswith((".exe", ".msi", ".deb" , "dmg")):
@@ -38,8 +49,8 @@ try:
             dir_name = "executables"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
         # Check if files are archive files
         elif filename.lower().endswith((".rar", ".tar" , ".zip" , ".gz")):
@@ -47,8 +58,8 @@ try:
             dir_name = "archive"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
 
         # Check if files are torrent files
@@ -57,8 +68,8 @@ try:
             dir_name = "torrent"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
         # Check if files are documents
         elif filename.lower().endswith((".txt", ".pdf", ".docx" , "doc")):
@@ -66,20 +77,9 @@ try:
             dir_name = "design-files"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
-
-        # Check if files are code files
-        elif filename.lower().endswith((".py", ".php", ".html" , ".css" , ".js")):
-            #Check if this file is Not The Script File itself
-            if not __file__.endswith(filename):
-                # If code folder doesnt exist then create
-                dir_name = "code"
-                if not os.path.exists(dir_name):
-                    os.makedirs(dir_name)
-                shutil.copy2(filename, dir_name)
-                os.remove(filename)
 
         # Check if files are design files
         elif filename.lower().endswith((".psd", ".ai")):
@@ -87,24 +87,22 @@ try:
             dir_name = "design-files"
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            shutil.move(filename, dir_name)
+            #os.remove(filename)
 
 
         # If file Doesn't Match any extension
         else :
-            dir_name = "others"
-            # If Others folder doesnt exist then create .
-            if not os.path.exists(dir_name):
-                os.makedirs(dir_name)
-            shutil.copy2(filename, dir_name)
-            os.remove(filename)
+            if os.path.isfile(filename):
+                dir_name = "others"
+                # If Others folder doesnt exist then create .
+                if not os.path.exists(dir_name):
+                    os.makedirs(dir_name)
+                shutil.move(filename, dir_name)
+                #os.remove(filename)'''
 
 
 
-except OSError:
-    print("Error happened ...... try again")
-finally:
-    # When script is finished clear screen and display message
-    os.system("cls" if os.name == "nt" else "clear")
+except Exception as e:
+    print("Error happened ...... try again" , e)
 print("Finished organising your files")
