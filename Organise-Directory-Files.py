9f0644e770ce23019cad9cdafbe10cf9a254dc2b
zakaria-90 <zak.omer.90@gmail.com>
2020-04-11 17:09:28 +0300

    elif

--- Organise-Directory-Files.py
+++ Organise-Directory-Files.py
@@ -8,76 +8,97 @@ try:
         # Check if files are images and you can add more extentions 
         if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".pbm", ".pnm")):
             # If images folder doesnt exist then create new folder
-            if not os.path.exists("images"):
-                os.makedirs("images")
-            shutil.copy2(filename, "images")
+            dir_name = "images"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are music and you can add more extentions
-        if filename.lower().endswith((".wav", ".mp3", ".flac", ".3gp", ".aa", ".aax", ".aiff", ".raw")):
+        elif filename.lower().endswith((".wav", ".mp3", ".flac", ".3gp", ".aa", ".aax", ".aiff", ".raw")):
             # If music folder doesnt exist then create
-            if not os.path.exists("music"):
-                os.makedirs("music")
-            shutil.copy2(filename, "music")
+            dir_name = "music"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are videos and you can add more extentions
-        if filename.lower().endswith((".webm", ".mp4")):
+        elif filename.lower().endswith((".webm", ".mp4")):
             # If video folder doesnt exist then create
-            if not os.path.exists("video"):
-                os.makedirs("video")
-            shutil.copy2(filename, "video")
+            dir_name = "video"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are executables
-        if filename.lower().endswith((".exe", ".msi", ".deb" , "dmg")):
+        elif filename.lower().endswith((".exe", ".msi", ".deb" , "dmg")):
             # If executables folder doesnt exist then create
-            if not os.path.exists("executables"):
-                os.makedirs("executables")
-            shutil.copy2(filename, "executables")
+            dir_name = "executables"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are archive files
-        if filename.lower().endswith((".rar", ".tar" , ".zip" , ".gz")):
+        elif filename.lower().endswith((".rar", ".tar" , ".zip" , ".gz")):
             # If archive folder doesnt exist then create
-            if not os.path.exists("archives"):
-                os.makedirs("archives")
-            shutil.copy2(filename, "archives")
+            dir_name = "archive"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
 
         # Check if files are torrent files
-        if filename.lower().endswith((".torrent",)):
+        elif filename.lower().endswith((".torrent",)):
             # If torrent folder doesnt exist then create
-            if not os.path.exists("torrent"):
-                os.makedirs("torrent")
-            shutil.copy2(filename, "torrent")
+            dir_name = "torrent"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are documents
-        if filename.lower().endswith((".txt", ".pdf", ".docx" , "doc")):
+        elif filename.lower().endswith((".txt", ".pdf", ".docx" , "doc")):
             # If documents folder doesnt exist then create
-            if not os.path.exists("documents"):
-                os.makedirs("documents")
-            shutil.copy2(filename, "documents")
+            dir_name = "design-files"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
 
         # Check if files are code files
-        if filename.lower().endswith((".py", ".php", ".html" , ".css" , ".js")):
+        elif filename.lower().endswith((".py", ".php", ".html" , ".css" , ".js")):
             # If code folder doesnt exist then create
-            if not os.path.exists("code"):
-                os.makedirs("code")
-            shutil.copy2(filename, "code")
+            dir_name = "code"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
 
         # Check if files are design files
-        if filename.lower().endswith((".psd", ".ai")):
+        elif filename.lower().endswith((".psd", ".ai")):
             # If desgin folder doesnt exist then create
-            if not os.path.exists("design-files"):
-                os.makedirs("design-files")
-            shutil.copy2(filename, "design-files")
+            dir_name = "design-files"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
             os.remove(filename)
+
+
+        # If file Doesn't Match any extension
+        else:
+            dir_name = "others"
+            # If Others folder doesnt exist then create
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            shutil.copy2(filename, dir_name)
+            os.remove(filename)
+
+
 
 except OSError:
     print("Error happened ...... try again")
